88e4de6702506b114018f77bb43107d7e343b420
Pawid-prianto27 <pawitanto27@gmail.com>
2020-01-11 07:36:39 -0800

    implementasi state pada inputan

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Deskripsi from  './component/deskripsi';
-import Input from './component/input';
+import Deskripsi from './component/deskripsi';
+
 
 class App extends Component {
 
   constructor(props){
     super(props)
+    this.state = {
+      name: "",
+    }
   }
 
+  setName = (e) => {
+    let name = e.target.value
+    this.setState({
+      name : name
+    })
+    console.log(name)
+  }
   render(){
       return (
         <div>
-        <Deskripsi name="priyanto"/>
-        <Input
-            name = "pawit"
-        />
+          <input onChange={this.setName}/>
+          <h3>{this.state.name}</h3>
+          <Deskripsi name={this.state.name}/>
         </div>
         );
       }
